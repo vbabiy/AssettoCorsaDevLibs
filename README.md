372ce535a8d474049baaf185573be0b53aa293e0
Turnermator13 <turnermator13@outlook.com>
2015-01-02 15:08:12 +0000

    init

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ AssettoCorsaDevLibs
 ===================
 
 Some Dev Libraries to aid AC app development 
+
+Adds ac functions into python to aid development in IDEs
+
+
+To install copy acDevLibs into "PythonXX\Lib\site-packages\"
+
+
+Then where you have "import ac" put:
+***
+try:
+    import ac
+except ImportError:
+    from acDevLibs.acDev import ac as ac
+***
+
+	
+For "import acsys" put:
+***
+try:
+    import acsys
+except ImportError:
+    from acDevLibs.acsysDev import acsys as acsys
+***
---
+++ acDevLibs/acsysDev.py
@@ -0,0 +1,96 @@
+class aeroDec:
+    CD = ""
+    CL_Front = ""
+    CL_Rear = ""
+
+
+class csDec:
+    AccG = ""
+    Aero = ""
+    BestLap = ""
+    Brake = ""
+    CGHeight = ""
+    CamberDeg = ""
+    CamberRad = ""
+    Caster = ""
+    Clutch = ""
+    CurrentTyresCoreTemp = ""
+    DY = ""
+    DriftBestLap = ""
+    DriftLastLap = ""
+    DriftPoints = ""
+    DriveTrainSpeed = ""
+    DynamicPressure = ""
+    Gas = ""
+    Gear = ""
+    InstantDrift = ""
+    IsDriftInvalid = ""
+    IsEngineLimiterOn = ""
+    LapCount = ""
+    LapInvalidated = ""
+    LapTime = ""
+    LastFF = ""
+    LastLap = ""
+    LastTyresTemp = ""
+    Load = ""
+    LocalAngularVelocity = ""
+    LocalVelocity = ""
+    Mz = ""
+    NdSlip = ""
+    NormalizedSplinePosition = ""
+    PerformanceMeter = ""
+    RPM = ""
+    RideHeight = ""
+    SlipAngle = ""
+    SlipAngleContactPatch = ""
+    SlipRatio = ""
+    SpeedKMH = ""
+    SpeedMPH = ""
+    SpeedMS = ""
+    SpeedTotal = ""
+    Steer = ""
+    SuspensionTravel = ""
+    ToeInDeg = ""
+    TurboBoost = ""
+    TyreContactNormal = ""
+    TyreContactPoint = ""
+    TyreDirtyLevel = ""
+    TyreHeadingVector = ""
+    TyreLoadedRadius = ""
+    TyreRadius = ""
+    TyreRightVector = ""
+    TyreSlip = ""
+    TyreSurfaceDef = ""
+    TyreVelocity = ""
+    Velocity = ""
+    WheelAngularSpeed = ""
+    WorldPosition = ""
+
+
+class glDec:
+    LineStrip = ""
+    Lines = ""
+    Quads = ""
+    Triangles = ""
+
+
+class vecDec:
+    def normalize(self, vector):
+        return 0
+
+
+class wheelsDec:
+    FL = ""
+    FR = ""
+    RL = ""
+    RR = ""
+
+
+class acsysDec:
+    AERO = aeroDec()
+    CS = csDec()
+    GL = glDec()
+    Vec2f = vecDec()
+    WHEELS = wheelsDec()
+
+acsys = acsysDec()
